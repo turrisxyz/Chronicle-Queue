69b3108f7978ad4f44cb94c34a8510cf5eecb0fc
Per Minborg <minborg@speedment.com>
2022-04-04 16:03:16 +0200

    Add ZeroCostTripWireTest

---
+++ src/test/java/net/openhft/chronicle/queue/ZeroCostTripWireTest.java
@@ -0,0 +1,13 @@
+package net.openhft.chronicle.queue;
+
+import net.openhft.chronicle.core.util.Ints;
+import org.junit.Test;
+
+public class ZeroCostTripWireTest {
+
+    @Test
+    public void trip() {
+        System.out.println("Ints.assertIfEnabled(i -> i > 0, -1) = " + Ints.assertIfEnabled(i -> i > 0, -1));
+    }
+
+}
